09369f0778c890c24b56af3ee5429f83ebc10120
meron847 <belachewmeron15@gmail.com>
2025-07-22 09:02:17 +0100

    even and odd

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+showNumbers(100);
+
+function showNumbers(limit){
+    for (let i= 0; i<= limit;i++){
+const message =(i % 2 === 0) ? 'Even' : 'odd';
+console.log(i,message);
+    }
+ 
+}
+    
