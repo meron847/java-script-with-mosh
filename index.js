76df5f9ad72be81816178fae6fd19a16354b8407
meron847 <belachewmeron15@gmail.com>
2025-07-23 11:03:27 +0100

    finding multiple of 3,5

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ function checkSpeed(speed){
     }
 
 
+    console.log(sum(10));
+
+function sum(limit){
+    let sum =0;
+
+ for(let i=0; i<= limit; i++)
+    if(i% 3 ===0 ||i % 5===0)
+        sum +=i;
+
+
+ return sum;
+}
+
 
 
 
