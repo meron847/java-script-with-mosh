bfe06e6dbb93bbb8007b484073c49286534af429
meron847 <belachewmeron15@gmail.com>
2025-07-24 10:34:36 +0100

    using for in

---
+++ For...in/index.js
@@ -0,0 +1,7 @@
+const colors=['red','green','yellow'];
+for (let index in colors)
+    console.log(index,colors[index]);
+
+const person={name:'ChannelMergerNode',age:'90'};
+for (let key in person)
+    console.log(key,person);
