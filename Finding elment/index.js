7b5b935ea829ae2346a335305238d2df1adeb678
meron847 <belachewmeron15@gmail.com>
2025-07-25 18:40:35 +0100

    finding primitiv

---
+++ Finding elment/index.js
@@ -0,0 +1,5 @@
+let numbers=[1,2,3,4,];
+console.log(numbers.indexof(1));
+console.log(numbers.lastIndexOf(4));
+//this line and the last line are same out put console.log(numbers.indexOf(1) !== -1);
+console.log(numbers.includes(1))
