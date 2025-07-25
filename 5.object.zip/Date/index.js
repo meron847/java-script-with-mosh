5433d238ecd0a189b929660f8caf35437fb3eafe
meron847 <belachewmeron15@gmail.com>
2025-07-25 15:16:38 +0100

    using date

---
+++ 5.object.zip/Date/index.js
@@ -0,0 +1,7 @@
+//Date is the constructor function
+const now= new Date()//in printesis it  can be empty 
+const date1 = new Date('MAy 11 2025 09:00');//in printesis it  can be value ,number
+const date2 = new Date(2025, 4,12,9)//in printesis it  can be year,month,date, houre,minute
+
+
+now.setFullYear(2024);
