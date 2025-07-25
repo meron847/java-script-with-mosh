4c2f5a09def32864241ce593c7a82d582a4092e0
meron847 <belachewmeron15@gmail.com>
2025-07-25 15:38:32 +0100

    using for in loop

---
+++ 5.object.zip/address obj/index.js
@@ -0,0 +1,10 @@
+const address={
+    street:'a',
+    city:'b',
+    zipCode:'c'
+};
+function showAddress(address){
+   for(let key in address)
+    console.log(key, address[key]);
+}
+showAddress(address)
