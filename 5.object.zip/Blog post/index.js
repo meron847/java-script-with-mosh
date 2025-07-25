5c73fd21b799a704201a8a83076750b343ff7151
meron847 <belachewmeron15@gmail.com>
2025-07-25 09:45:58 +0100

    blog

---
+++ 5.object.zip/Blog post/index.js
@@ -0,0 +1,12 @@
+let post={
+    title:'a',
+    body:'b',
+    author:'c',
+    view:90,
+    comments:[ 
+        {author:'a',body:'b'},
+        {author:'c',body:'d'}
+    ],
+isLive: true
+};
+console.log(post);
