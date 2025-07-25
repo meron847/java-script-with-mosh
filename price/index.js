cc3d8e53a0e03e8308c8afe90b58912d9b71a8a3
meron847 <belachewmeron15@gmail.com>
2025-07-25 17:26:00 +0100

    price

---
+++ price/index.js
@@ -0,0 +1,6 @@
+const priceRange=[
+    {label:$, tooltip:Inexpensive,minPerPerson:0,maxPerPerson:10},
+    {label:$$, tooltip:Moderate,minPerPerson:10,maxPerPerson:20},
+    {label:$$$, tooltip:Expensive,minPerPerson:20,maxPerPerson:100}
+];
+let restarants=[{averagePerperson:5}]
